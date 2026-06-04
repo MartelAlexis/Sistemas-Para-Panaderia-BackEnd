1e406269f34b175d94fd9e896ba49614db2b75d0
evansguevarag <aljama369@gmail.com>
2026-06-03 22:08:00 -0500

    feat(product): Implementar el endpoint de creación de categoría en categoryController

--- src/main/java/com/Sistemas_Para_Panaderia_BackEnd/controllers/CategoryController.java
+++ src/main/java/com/Sistemas_Para_Panaderia_BackEnd/controllers/CategoryController.java
@@ -4,6 +4,7 @@ import com.Sistemas_Para_Panaderia_BackEnd.dtos.CategoryDTO;
 import com.Sistemas_Para_Panaderia_BackEnd.services.CategoryService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -20,5 +21,10 @@ public class CategoryController {
     @GetMapping({ "", "/" })
     public ResponseEntity<List<CategoryDTO>> getAllCategories() {
         return ResponseEntity.ok(categoryService.getAllCategories());
+    }
+
+    @PostMapping({ "", "/" })
+    public ResponseEntity<CategoryDTO> createCategory(@RequestBody CategoryDTO request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.createCategory(request));
     }
 }
